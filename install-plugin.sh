b7214fd368495f51bfcd0b61c0ee89d255a01cec
sigefried <sigefriedhyy@gmail.com>
2017-02-24 15:05:44 +0000

    fix install-plugin.sh script

--- install-plugin.sh
+++ install-plugin.sh
@@ -5,13 +5,25 @@
 #
 # Distributed under terms of the MIT license.
 
-mkdir ~/bin
+if [ ! -e ~/bin ]
+then
+	mkdir ~/bin
+fi
 
 $CURRENT_DIR=$(pwd)
 
 # install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/instal
+if [ ! -e ~/.fzf ]
+then
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
+fi
 
 # install enhancd
-git clone https://github.com/b4b4r07/enhancd
+if [ ! -e ~/bin/enhancd ]
+then
+	git clone https://github.com/b4b4r07/enhancd ~/bin/enhancd
+fi
+
+echo "plut install complete"
+
