295108b167d098e90a7e4503717bf4cb3c8c3bbd
Yuyang Huang <sigefriedhyy@gmail.com>
2025-06-13 21:40:52 +0900

    update

--- install-cargo-plugin.sh
+++ install-cargo-plugin.sh
@@ -5,15 +5,20 @@
 #
 # Distributed under terms of the MIT license.
 
+set -e
 
-# install tools using cargo
-if [ ! -z $(which cargo) ]
-then
-  cargo install bat
-  cargo install exa
-  cargo install tokei
-  cargo install procs
-  cargo install ripgrep
-  cargo install zoxide
+# Check if cargo is available, install Rust if not
+if ! command -v cargo &> /dev/null; then
+  echo "Rust is not installed. Installing via rustup..."
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  # Source environment (use correct location depending on shell)
+  source "$HOME/.cargo/env"
 fi
+
+cargo install bat
+cargo install exa
+cargo install tokei
+cargo install procs
+cargo install ripgrep
+cargo install zoxide
 
