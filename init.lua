9367ac92a95b1d63e955c24f8a094971278bd3cc
Yuyang Huang <sigefriedhyy@gmail.com>
2025-09-20 11:58:50 +0900

    remove deprecate config

--- init.lua
+++ init.lua
@@ -229,16 +229,6 @@ require("lazy").setup({
             { name = "path" },
           }),
         })
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        require('lspconfig').clangd.setup {
-          capabilities = capabilities,
-        }
-        require('lspconfig').pyright.setup {
-          capabilities = capabilities,
-        }
-        require('lspconfig').ts_ls.setup {
-          capabilities = capabilities,
-        }
       end,
     },
     {
